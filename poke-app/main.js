6124bf6b201a4a251ba10ccaf809970af4555d44
brooklin <arguetgd@gmail.com>
2024-03-08 20:49:09 +0100

    mi primer lab

---
+++ poke-app/main.js
@@ -0,0 +1,35 @@
+import './style.css'
+import fetchPokemon from './src/services/api.js';
+
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+const pokemonDisplay = document.getElementById('pokemon-display');
+const pokemonD= document.getElementById('poke');
+
+searchButton.addEventListener('click', async () => {
+    const query = searchInput.value.trim();
+    if (!query) return; // No buscar si el input está vacío.
+    console.log('test');
+    try {
+        const pokemon = await fetchPokemon('https://pokeapi.co/api/v2/pokemon/', query);
+        console.log(displayPokemonSprites);
+       displayPokemon(pokemon);
+       displayPokemonSprites(pokemon) ;
+ } catch (error) {
+        console.error("Error al buscar el Pokémon:", error);
+    }
+});
+
+const  displayPokemon = (pokemon) => {
+    pokemonDisplay.innerHTML = ' ';
+    const pokemonName = document.createElement('h2');
+    pokemonName.textContent = pokemon.name;
+    pokemonDisplay.appendChild(pokemonName);
+}
+const displayPokemonSprites= (pokemon) =>{
+  pokemonD.innerHTML=' ' ;
+  const pokemonName = document.createElement('img');
+  pokemonName.src=pokemon.sprites.front_default;
+  pokemonD.appendChild(pokemonName);
+
+}
